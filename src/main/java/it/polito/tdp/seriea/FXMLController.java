3db3ff9e881c6c5a0c069189781bd32b0502b5c4
claudiazingarello <s238072@studenti.polito.it>
2020-06-23 23:41:52 +0200

    b.completato

--- src/main/java/it/polito/tdp/seriea/FXMLController.java
+++ src/main/java/it/polito/tdp/seriea/FXMLController.java
@@ -1,9 +1,12 @@
 package it.polito.tdp.seriea;
 
 import java.net.URL;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 import it.polito.tdp.seriea.model.Model;
+import it.polito.tdp.seriea.model.Season;
+import it.polito.tdp.seriea.model.Team;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -11,56 +14,76 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 
 public class FXMLController {
-	
+
 	private Model model;
 
-    @FXML
-    private ResourceBundle resources;
+	@FXML
+	private ResourceBundle resources;
 
-    @FXML
-    private URL location;
+	@FXML
+	private URL location;
 
-    @FXML
-    private ChoiceBox<?> boxSquadra;
+	@FXML
+	private ChoiceBox<Team> boxSquadra;
 
-    @FXML
-    private Button btnSelezionaSquadra;
+	@FXML
+	private Button btnSelezionaSquadra;
 
-    @FXML
-    private Button btnTrovaAnnataOro;
+	@FXML
+	private Button btnTrovaAnnataOro;
 
-    @FXML
-    private Button btnTrovaCamminoVirtuoso;
+	@FXML
+	private Button btnTrovaCamminoVirtuoso;
 
-    @FXML
-    private TextArea txtResult;
+	@FXML
+	private TextArea txtResult;
 
-    @FXML
-    void doSelezionaSquadra(ActionEvent event) {
+	@FXML
+	void doSelezionaSquadra(ActionEvent event) {
+		txtResult.clear();
+		Team t = boxSquadra.getValue() ;
 
-    }
+		if(t==null) {
+			txtResult.appendText("Devi selezionare una squadra\n");
+			return ;
+		}
 
-    @FXML
-    void doTrovaAnnataOro(ActionEvent event) {
+		Map<Season, Integer> punteggi = model.calcolaPunteggi(t) ;
 
-    }
+		txtResult.clear();
 
-    @FXML
-    void doTrovaCamminoVirtuoso(ActionEvent event) {
+		for(Season s: punteggi.keySet()) {
+			txtResult.appendText(String.format("%s: %d\n", s.getDescription(), punteggi.get(s)) );
+		}
 
-    }
+		btnTrovaAnnataOro.setDisable(false);
+		btnTrovaCamminoVirtuoso.setDisable(true);
+	}
 
-    @FXML
-    void initialize() {
-        assert boxSquadra != null : "fx:id=\"boxSquadra\" was not injected: check your FXML file 'SerieA.fxml'.";
-        assert btnSelezionaSquadra != null : "fx:id=\"btnSelezionaSquadra\" was not injected: check your FXML file 'SerieA.fxml'.";
-        assert btnTrovaAnnataOro != null : "fx:id=\"btnTrovaAnnataOro\" was not injected: check your FXML file 'SerieA.fxml'.";
-        assert btnTrovaCamminoVirtuoso != null : "fx:id=\"btnTrovaCamminoVirtuoso\" was not injected: check your FXML file 'SerieA.fxml'.";
-        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'SerieA.fxml'.";
+	@FXML
+	void doTrovaAnnataOro(ActionEvent event) {
 
-    }
+	}
+
+	@FXML
+	void doTrovaCamminoVirtuoso(ActionEvent event) {
+
+	}
+
+	@FXML
+	void initialize() {
+		assert boxSquadra != null : "fx:id=\"boxSquadra\" was not injected: check your FXML file 'SerieA.fxml'.";
+		assert btnSelezionaSquadra != null : "fx:id=\"btnSelezionaSquadra\" was not injected: check your FXML file 'SerieA.fxml'.";
+		assert btnTrovaAnnataOro != null : "fx:id=\"btnTrovaAnnataOro\" was not injected: check your FXML file 'SerieA.fxml'.";
+		assert btnTrovaCamminoVirtuoso != null : "fx:id=\"btnTrovaCamminoVirtuoso\" was not injected: check your FXML file 'SerieA.fxml'.";
+		assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'SerieA.fxml'.";
+
+	}
 
 	public void setModel(Model model) {
 		this.model = model;
+
+		boxSquadra.getItems().clear();
+		boxSquadra.getItems().addAll(model.getSquadre());
 	}
 }
--- src/main/java/it/polito/tdp/seriea/model/Model.java
+++ src/main/java/it/polito/tdp/seriea/model/Model.java
@@ -1,5 +1,88 @@
 package it.polito.tdp.seriea.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleDirectedWeightedGraph;
+
+import it.polito.tdp.seriea.db.SerieADAO;
+
 public class Model {
 
+	private List<Team> squadre;
+	private List<Season> stagioni;
+	private Map<String, Team> squadreIdMap;
+	private Map<Integer, Season> stagioniIdMap;
+
+	private Team squadraSelezionata;
+	private Map<Season, Integer> punteggi;
+	private List<Season> stagioniConsecutive;
+
+	private Graph<Season, DefaultWeightedEdge> grafo;
+
+	private List<Season> percorsoBest;
+	private int deltaPesi;
+
+	public Model() {
+		SerieADAO dao = new SerieADAO();
+
+		this.squadre = dao.listTeams();
+		this.squadreIdMap = new HashMap<String, Team>();
+		for (Team t : this.squadre) {
+			this.squadreIdMap.put(t.getTeam(), t);
+		}
+
+		this.stagioni = dao.listAllSeasons();
+		this.stagioniIdMap = new HashMap<Integer, Season>();
+		for (Season s : this.stagioni) {
+			this.stagioniIdMap.put(s.getSeason(), s);
+		}
+
+	}
+
+	public List<Team> getSquadre() {
+		return this.squadre;
+	}
+
+	public Map<Season, Integer> calcolaPunteggi(Team squadra) {
+
+		this.squadraSelezionata = squadra;
+
+		this.punteggi = new HashMap<Season, Integer>();
+
+		SerieADAO dao = new SerieADAO();
+
+		List<Match> partite = dao.listMatchesForTeam(squadra, stagioniIdMap, squadreIdMap);
+
+		for (Match m : partite) {
+
+			Season stagione = m.getSeason();
+
+			int punti = 0;
+
+			if (m.getFtr().equals("D")) {
+				punti = 1;
+			} else {
+				if ((m.getHomeTeam().equals(squadra) && m.getFtr().equals("H"))
+						|| (m.getAwayTeam().equals(squadra) && m.getFtr().equals("A"))) {
+					punti = 3;
+				}
+			}
+
+			Integer attuale = punteggi.get(stagione);
+			if (attuale == null)
+				attuale = 0;
+			punteggi.put(stagione, attuale + punti);
+
+		}
+
+		return punteggi;
+
+	}
 }
